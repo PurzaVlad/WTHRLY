dc7dcfe415f6f72568d24c3d0f4c6332f36222cb
Purza Vlad <63791382+PurzaVlad@users.noreply.github.com>
2024-07-27 20:33:51 +0300

    Update views.py

--- site/views.py
+++ site/views.py
@@ -1,14 +1,12 @@
-#viewwthrly
 from django import forms
 from django.shortcuts import render, redirect
 from django.urls import reverse
-from django.utils.text import slugify
 
 orase = ["Timisoara", "Arad", "caca"]
 
 class CityForm(forms.Form):
     oras=forms.CharField(label="", required=False, widget=forms.TextInput(attrs={
-            'class': 'form-control',
+            'class': 'form-control rounded-pill',
             'placeholder': 'Enter city name'
         }))
 
@@ -17,18 +15,16 @@ def index(request):
         form = CityForm(request.GET)
         if form.is_valid():
             oras = form.cleaned_data["oras"]
-            oras_slug = slugify(oras)
-            if oras_slug in [slugify(city) for city in orase]:
-                return redirect(reverse("wthrlyapp:localitate", args=[oras_slug]))
+            if oras in orase:
+                return redirect(reverse("wthrlyapp:localitate", args=[oras]))
             else:
                 return render(request,"wthrlyapp/index.html", {"form":form})
 
-
-    else:
-        form = CityForm()
     return render(request,"wthrlyapp/index.html", {"form":form})
 
 def localitate(request, oras):
     return render(request, "wthrlyapp/localitate.html", {
                   "oras" : oras
                   })
+
+#apikey:92a76a09a521f40e970e466cfbdf2432
